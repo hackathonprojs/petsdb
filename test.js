1fd765adc1bb9b8b0455c990baf8bb422aa05bb5
swswsw <apiswswsw@gmail.com>
2019-10-03 14:39:52 -0700

    intermediate

---
+++ test.js
@@ -0,0 +1,13 @@
+let test = {
+  animals: [
+    {"t":"1"},
+    {"t":"1"},
+    // {"t":"1"},
+    // {"t":"1"},
+    // {"t":"1"},
+  ]
+}
+
+test.animals.forEach(elem => {
+  console.log(elem);
+})
---
+++ writeJsonData.js
@@ -0,0 +1,26 @@
+const irisnative = require('intersystems-iris-native')
+const jsonData = require('petJson');
+
+
+// Modify connection info based on environment
+let connectionInfo = {
+    host: '127.0.0.1', 
+    port: 5001,
+    ns: 'USER', 
+    user: 'superuser',
+    pwd: 'sys' 
+};
+// create database connection
+const connection = irisnative.createConnection(connectionInfo);
+
+//create IRIS instance
+const dbnative = connection.createIris();
+
+console.log('writing the data into intersystems db from a json data');
+
+jsonData.json.forEach(elem => {
+  console.log(elem);
+});
+
+// close connection
+connection.close();
